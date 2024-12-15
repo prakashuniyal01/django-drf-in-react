4eb8e4ce527970f39bc4d78a11323b2a520329c9
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-12-15 16:06:24 +0530

    feat: build submitArticle component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import ArticleSubmissionForm from "./SubmitArticle";
 
 const HomePage = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-center">Welcome to My App</h1>
-      <p className="mt-4 text-center">
-        This is the home page of your application. Feel free to explore!
-      </p>
+      <ArticleSubmissionForm />
     </div>
   );
 };
